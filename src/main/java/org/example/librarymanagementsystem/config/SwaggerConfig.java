aa1f364319fe32e3ef3507e629e720c7ea292caa
44J-jottabyte <elgynkhalilzade@gmail.com>
2025-03-27 22:15:15 +0400

    package name refactor

--- src/main/java/org/example/librarymanagementsystem/config/SwaggerConfig.java
+++ src/main/java/org/example/librarymanagementsystem/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package project.bookmanagement.config;
+package org.example.librarymanagementsystem.config;
 
 import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
